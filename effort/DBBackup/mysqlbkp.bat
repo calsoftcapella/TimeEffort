776c378991c6f978fa69a9e764c5e2c732612557
calsoftcapella <thangamurugans@calsoftlabs.com>
2015-02-16 10:47:41 +0530

    initial config commits.

---
+++ effort/DBBackup/mysqlbkp.bat
@@ -0,0 +1,3 @@
+C:
+CD C:\Program Files\MySQL\MySQL Server 5.1\bin
+mysqldump -uroot -pcalsoft --all-databases > D:\TimeEffort\DBBackup\medecision.sql
---
+++ effort/README.txt
@@ -0,0 +1,4 @@
+Java: JDK6U43
+Server: GlassFish-3.1.2
+WorkBench: MySQL Workbench 5.2.47 CE
+DB: mysql-essentials-5.1.73-win32
